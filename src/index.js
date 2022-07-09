5a5b8344308df84d3e115e2ee7e3af7dad697011
juandiaco <85654035+juandiaco@users.noreply.github.com>
2022-07-09 14:22:50 -0300

    Edición de usuario + bd

--- src/index.js
+++ src/index.js
@@ -93,7 +93,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 // PRUEBA SARASA
 app.use(function (req, res, next){
     res.header("Access-Control-Allow-Origin", "http://localhost:3000");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Methods");
     res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
     next();
 });
